0697e8528bdadc8624569fbf60991e750af6d49d
Aleksandr Fesenko <fesenkoas@gmail.com>
2024-03-05 07:10:01 +0200

    new

--- client/src/App.js
+++ client/src/App.js
@@ -9,33 +9,23 @@ function App() {
   
 
   useEffect(() => {
-    // Создаем новое подключение WebSocket
+  
     const socket = new WebSocket("ws://localhost:3001");
-
-    // Обработчик события открытия соединения
+   
     socket.addEventListener("open", function (event) {
       console.log("WebSocket соединение установлено");
     });
-
-    // Обработчик события получения сообщения
     socket.addEventListener("message", function (event) {
-      // Распаковываем данные из JSON
       const data = JSON.parse(event.data);
       data.path.map((item, key) => setMsg((prevMsg) => [...prevMsg, item]));
-      // Делаем что-то с полученными данными
       console.log("Получено сообщение:", data);
     });
-    // Обработчик события закрытия соединения
     socket.addEventListener("close", function (event) {
       console.log("WebSocket соединение закрыто");
     });
-
-    // Обработчик события ошибки
     socket.addEventListener("error", function (event) {
       console.error("WebSocket ошибка:", event);
     });
-
-    // Очистка эффекта: закрытие соединения при размонтировании компонента
     return () => {
       socket.close();
     };
