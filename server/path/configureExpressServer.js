3f76ae2ed5bfcace1ca7cf9900c4e0b2149801a4
Aleksandr Fesenko <fesenkoas@gmail.com>
2024-03-10 06:22:41 +0200

    09.03

--- server/path/configureExpressServer.js
+++ server/path/configureExpressServer.js
@@ -5,10 +5,12 @@ export const configureExpressServer = (app, server) => {
  
   const buildPath = path.resolve(path.join(process.cwd(), '../public', 'build'));
   app.use(express.static(buildPath));
+
   app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
   });
+  
   app.get('*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
