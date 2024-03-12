dad4087595adb38c5eb15f0e8ba4d66660c487e4
Aleksandr Fesenko <fesenkoas@gmail.com>
2024-03-12 18:16:39 +0200

    12.03

--- server/util/parseURL.js
+++ server/util/parseURL.js
@@ -1,36 +1,37 @@
-
 export const parseUrl = (filePath, folderPath, fileName) => {
-  
- const folders = folderPath.split("\\");
-  
- const ripIndex = folders.indexOf("rip");
- 
- const extractedFolders = ripIndex !== -1 && ripIndex < folders.length - 1 ? folders.slice(ripIndex + 1) : [];
+  const folders = folderPath.split("\\");
 
- const regex = /(\d+)\s(.+?)\s(\d+)x(\d+)\s?cm\s?(X(\d+))?\.prt/;
- 
- const match = fileName.match(regex);
+  const ripIndex = folders.indexOf("rip");
 
- let height = 0;
- let width = 0;
- let col = 1; 
- if (match) {
-   height = match[3];
-   width = match[4];
-   col = match[6] ? parseInt(match[6]) : 1; 
- }
+  const extractedFolders =
+    ripIndex !== -1 && ripIndex < folders.length - 1
+      ? folders.slice(ripIndex + 1)
+      : [];
 
- if (extractedFolders.length ) {
-   return {
-     url: filePath,
-     folderDate: extractedFolders[0],
-     folderFabric: extractedFolders[1] || "Specify fabric",
-     name: fileName,
-     height,
-     width,
-     col,
-   };
- } else {
-   return -1;
- }
+  const regex = /(\d+)\s(.+?)\s(\d+)x(\d+)\s?cm\s?(X(\d+))?\.prt/;
+
+  const match = fileName.match(regex);
+
+  let height = 0;
+  let width = 0;
+  let col = 1;
+  if (match) {
+    height = match[3];
+    width = match[4];
+    col = match[6] ? parseInt(match[6]) : 1;
   }
+
+  if (extractedFolders.length > 0) {
+    return {
+      url: filePath,
+      folderDate: extractedFolders[0],
+      folderFabric: extractedFolders[1],
+      name: fileName,
+      height,
+      width,
+      col,
+    };
+  } else {
+    return -1;
+  }
+};
