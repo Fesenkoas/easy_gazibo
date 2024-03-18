6e4d66001afaa4e124f4b18335e345f1b3efaae8
Aleksandr Fesenko <fesenkoas@gmail.com>
2024-03-18 21:34:05 +0200

    18.03

--- client/src/components/ShowItem.js
+++ client/src/components/ShowItem.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React from 'react'
 
-export const ShowItem = ({ url, setShowItems }) => {
-  const newFilePath = url.replace(/\.prt$/, ".jpg");
-  console.log(newFilePath);
+export const ShowItem = ({url, setShowItems}) => {
+    const newFilePath = url.replace(/\.prt$/, ".jpg");
+    console.log(url);
   return (
-    <div className="fixed w-[600px] h-[500px]  bg-white m-auto">
-      <div>
-        <img
-        src={'file:///C:/Users/Fesenko/Desktop/rip/14.03/Flag/10 153x80cm X8.jpg'}
+    <div className="fixed w-[600px]  bg-white m-auto">
+    <div>
+      <img
+        src={newFilePath}
         alt=""
         onClick={() => setShowItems(false)}
       />
-      </div>
+      
     </div>
-  );
-};
+  </div>
+  )
+}
